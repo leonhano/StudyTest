b37237c5fb448c554d2ae74b97b8007c0fccf19f
Hao Han <hao1.han@bmo.com>
2019-09-25 15:17:10 -0400

    4-3

---
+++ CH4/src/app/shared/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  getProduct(): Product {
+    return new Product(0, 'test', 5888, 'test description');
+  }
+}
+
+
+export class Product {
+  constructor(public id: number, public title: string, public price: number, public desc: string) {
+    
+  }
+}
